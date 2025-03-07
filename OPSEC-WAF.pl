5117630fcdafe47f7755425608dd0414ef08a809
nu11secur1ty <venvaropt@gmail.com>
2025-03-07 10:17:56 +0200

    Update OPSEC-WAF.pl

--- OPSEC-WAF.pl
+++ OPSEC-WAF.pl
@@ -7,13 +7,13 @@ use diagnostics;
 # Deploy htaccess module
 print "Type Your path of root doc on your Apache...\n";
     my $dir = <STDIN>;
-    my $inst = `cp /modules/.htaccess $dir`;
+    my $inst = `cp modules/.htaccess $dir`;
     
 # Deploy Apache conf    
 print "Type the real path of your httpd.conf...\n";    
     my $httpd = <STDIN>;
-    my $inst_httpd = `cp /modules/httpd.conf $httpd`;
-    my $inst_httpd_apache2 = `cp /modules/apache2.conf $httpd`;
+    my $inst_httpd = `cp modules/httpd.conf $httpd`;
+    my $inst_httpd_apache2 = `cp modules/apache2.conf $httpd`;
 
     
 print "All is done\n";
